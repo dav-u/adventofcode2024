6c5dac0f02f5245501ee7418690d456aa6d6a35c
David <24206701+dav-u@users.noreply.github.com>
2024-12-02 11:41:16 +0100

    day02_part1

---
+++ python/day02/part1.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+
+reports = [np.array(l.split(), dtype='int') for l in lines]
+
+correct_count = 0
+
+for report in reports:
+    diffs = np.diff(report)
+
+    # each difference is either strictly positive or negative
+    is_strictly_monotonic = np.all(diffs < 0) | np.all(diffs > 0)
+
+    # the absolute differences have to lie between 1 and 3 (including)
+    abs_diffs = np.abs(diffs)
+    correct_diffs = (abs_diffs.max() <= 3) & (abs_diffs.min() >= 1)
+
+    is_correct = is_strictly_monotonic & correct_diffs
+    # print(report, diffs, is_strictly_monotonic, correct_diffs, is_correct)
+
+    if is_correct:
+        correct_count += 1
+
+print(correct_count)
